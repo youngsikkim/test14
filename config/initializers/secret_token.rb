ab7c5c978364047b3f2d759db7e65bbfd2630faa
youngsik.kim <youngsik.kim@headflow.net>
2012-04-22 22:29:25 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test14::Application.config.secret_token = '4116129d79a614754ef37d19f2161bf8fef52e344d8468cd3cf13452d7bb6642f7b56855bf18b22b934d391512b17436f31f597acabc73863dfbcfc7b8c8c05b'
